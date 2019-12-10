36b655b53121fb21678d35f703b5971a33f6d70a
Blaine93 <dsi_93@live.com>
2019-12-10 14:59:25 +0300

    added example with space background, space carousel image added to assets

--- example/src/App.js
+++ example/src/App.js
@@ -7,6 +7,29 @@ export default class App extends Component {
     return (
       <div className="wrapper">
         <h1>React Pure Carousel</h1>
+        <div className="demo-carousel">
+          <Carousel>
+            <Carousel.Background src="https://cdn.dodowallpaper.com/full/671/wallpaper-outer-space-1.jpg" />
+            <Carousel.Item>
+              <Carousel.Caption alignItems="bottom">
+                <h2>Carousel with Space Background</h2>
+                <p>Made by React Pure Carousel</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Carousel.Caption alignItems="bottom">
+                <h2>Carousel with Space Background</h2>
+                <p>Made by React Pure Carousel</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Carousel.Caption alignItems="bottom">
+                <h2>Carousel with Space Background</h2>
+                <p>Made by React Pure Carousel</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
         <div className="carousel">
           <Carousel>
             <Carousel.Background src={bgImg} />
